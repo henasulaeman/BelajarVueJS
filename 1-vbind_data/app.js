c2d79c75d65cfe6453d69dd8822e626b167c9df6
unknown <henasulaeman@sttbandung.ac.id>
2024-01-20 11:36:57 +0700

    first learn

---
+++ 1-vbind_data/app.js
@@ -0,0 +1,12 @@
+var product = {
+    "name":"Pompa Air Portable",
+    "deskripsi":"preasure 320 PSI 22 BAR",
+    "price":"100.000",
+    "image":"https://partojambe.com/asset/foto_produk/POMPA_AIR_3.PNG"
+}
+var app = new Vue({
+    el: '#app',
+    data:{
+        name: product
+    }
+});
