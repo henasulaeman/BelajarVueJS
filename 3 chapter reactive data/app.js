0c35fe87e9ee3be5635b06fba229b0104e2fda0f
oasis.sttbandung@gmail.com <oasis.sttbandung@gmail.com>
2024-02-07 06:38:21 +0700

    Bismillah

---
+++ 3 chapter reactive data/app.js
@@ -0,0 +1,10 @@
+var product ={
+    "name":"Bamboo Thermal Ski Coat",
+    "description":"ini adalah deskripsi produk",
+    "price":"99",
+    "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+}
+var app = new Vue({
+    el:'#app',
+    data: product
+});
