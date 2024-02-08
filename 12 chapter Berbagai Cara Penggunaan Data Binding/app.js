8c4406d62558c1721adf0a090be2e24d90478f5f
oasis.sttbandung@gmail.com <oasis.sttbandung@gmail.com>
2024-02-08 22:31:17 +0700

    tetap semangat

---
+++ 12 chapter Berbagai Cara Penggunaan Data Binding/app.js
@@ -0,0 +1,16 @@
+// var product ={
+//     "name":"Bamboo Thermal Ski Coat",
+//     "description":"ini adalah deskripsi produk",
+//     "price":"99",
+//     "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+// }
+
+
+
+var app = new Vue({
+    el:'#app',
+    data: {
+        name:"Vue JS"   
+    },
+  
+});
---
+++ 9 chapter Event dan Method/app.js
@@ -0,0 +1,33 @@
+// var product ={
+//     "name":"Bamboo Thermal Ski Coat",
+//     "description":"ini adalah deskripsi produk",
+//     "price":"99",
+//     "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+// }
+
+
+
+var app = new Vue({
+    el:'#app',
+    data: {
+        maximum:50,
+        products: null,
+        cart: []
+    },
+    // Fungsi event mounted adalah ketika di deklerasikan maka code yang 
+    // di dalam mounted akan secara otomatis terload
+    // Even yang terjadi pada saat objek di buat dan template sudah siap di render
+    mounted: function() {
+        fetch('https://hplussport.com/api/products/order/price')
+        .then(response => response.json())
+        .then(data => {
+            this.products = data;
+        });
+    },
+    // Contoh Method
+    methods: {
+        addItem: function (product){
+            this.cart.push(product);
+        }
+    }
+});
